9cd22f5f4952001dff3313aef0e4dc7c9b12f1a6
Vivas Pocostales Jorge <jovivaspo@gmail.com>
2022-01-09 18:20:25 +0100

    first steps

---
+++ gameback/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Button } from 'react-bootstrap'
+function App() {
+  return (
+    <div className='main  m-25 bg-primary'>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
