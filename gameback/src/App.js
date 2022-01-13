988959a961d29d9bb91f14b6d3c5d65779f9f16b
Vivas Pocostales Jorge <jovivaspo@gmail.com>
2022-01-13 14:58:46 +0100

    base-url

--- gameback/src/App.js
+++ gameback/src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import NavBar from './components/NavBar';
 function App() {
   return (
-    <Router basename='/gameback_frontend'>
+    <Router>
       <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
